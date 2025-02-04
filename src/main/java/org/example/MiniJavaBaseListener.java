5958fe9cd500a3e325bf3b4066b5baec905ca049
s.soltanian <s.soltaniant@gmail.com>
2025-02-04 21:17:47 +0330

    refactor generated codes

--- src/main/java/org/example/MiniJavaBaseListener.java
+++ src/main/java/org/example/MiniJavaBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from MiniJava.g4 by ANTLR 4.9.2
+package org.example; // Generated from MiniJava.g4 by ANTLR 4.9.2
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
--- src/main/java/org/example/MiniJavaLexer.java
+++ src/main/java/org/example/MiniJavaLexer.java
@@ -1,12 +1,10 @@
-// Generated from MiniJava.g4 by ANTLR 4.9.2
+package org.example; // Generated from MiniJava.g4 by ANTLR 4.9.2
+
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.atn.*;
 import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class MiniJavaLexer extends Lexer {
