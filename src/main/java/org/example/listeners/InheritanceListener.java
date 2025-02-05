b818f7e5937f0b732b6fd4a1509251485f0ab95d
s.soltanian <s.soltaniant@gmail.com>
2025-02-05 08:09:03 +0330

    add InheritanceListener

---
+++ src/main/java/org/example/listeners/InheritanceListener.java
@@ -0,0 +1,46 @@
+package org.example.listeners;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.example.MiniJavaBaseListener;
+import org.example.MiniJavaParser;
+import org.example.symboltable.SymbolTable;
+import org.example.symboltable.records.Class;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class InheritanceListener extends MiniJavaBaseListener {
+
+    private final SymbolTable symbolTable;
+
+    /**
+     * By entering a class declaration we will check for inheritance, so that if it extends any other
+     * class, we will add parent classes' methods and global variables to current class.
+     * <p>
+     * Note: this method will also check for cyclic inheritance.
+     */
+    @Override
+    public void enterClassDeclaration(MiniJavaParser.ClassDeclarationContext ctx) {
+        String id = ctx.Identifier(0).getText();
+        Class currClass = symbolTable.findClass(id);
+        if (ctx.getText().contains("extend")) {
+            String parentClassId = ctx.Identifier(1).getText();
+            List<String> parents = new ArrayList<>();
+
+            while (parentClassId != null) {
+                if (parents.contains(parentClassId))
+                    SymbolTableCreator.printError("ERROR: Cyclic inheritance is not allowed");
+
+                parents.add(parentClassId);
+
+                Class parent = symbolTable.getProgram().getClasses().get(parentClassId);
+                parent.getGlobalVars().forEach((k,v) -> currClass.getGlobalVars().putIfAbsent(k, v));
+                parent.getMethods().forEach((k,v) -> currClass.getMethods().putIfAbsent(k, v));
+                parentClassId = parent.getParentClassId();
+            }
+        }
+    }
+}
