013c800d367649da73598c3b96372b2b176c9aec
s.soltanian <s.soltaniant@gmail.com>
2025-02-05 08:10:19 +0330

    update main to run samples

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,10 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.example.listeners.InheritanceListener;
+import org.example.listeners.ScopeCheckingStatements;
+import org.example.listeners.SymbolTableCreator;
+import org.example.listeners.TypeChecker;
 import org.example.utils.FileReaderUtil;
 
 import java.io.*;
@@ -17,10 +21,22 @@ public class Main {
         MiniJavaLexer lexer = new MiniJavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiniJavaParser parser = new MiniJavaParser(tokens);
-        ParseTree tree = parser.goal();
+        ParseTree tree = parser.program();
 
-        CustomListener listener = new CustomListener();
-        ParseTreeWalker.DEFAULT.walk(listener, tree);
-        System.out.println(listener.symbolTable.table);
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        SymbolTableCreator sbBuilder = new SymbolTableCreator(parser);
+        walker.walk(sbBuilder, tree);
+
+        InheritanceListener inheritance = new InheritanceListener(sbBuilder.getSymbolTable());
+        walker.walk(inheritance, tree);
+
+        ScopeCheckingStatements verifier = new ScopeCheckingStatements(inheritance.getSymbolTable());
+
+        walker.walk(verifier, tree);
+        verifier.getSymbolTable().printSymbolTable();
+
+//        TypeChecker typecheck = new TypeChecker(verifier.getSymbolTable());
+//        walker.walk(typecheck, tree);
     }
 }
